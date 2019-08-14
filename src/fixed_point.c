c5f2a3b27651cec87197dcfab57cba4b0add584e
Christian Fulljames <christianfulljames@gmail.com>
2019-08-13 21:28:25 -0400

    Add simple fixed point implementation
    This will be used by the oscillator and potentially other parts of the
    application in the future.  Currently it is quite simple; it may need to
    be expanded to be more generically useful.

---
+++ src/fixed_point.c
@@ -0,0 +1,8 @@
+#include "fixed_point.h"
+
+/*
+ * This library currently does not have any functions to implement; all
+ * functionality is contained in the header macros.  It may be expanded in the
+ * future with functions for more math operations if the application requires
+ * them, at which point this comment should be removed.
+ */
