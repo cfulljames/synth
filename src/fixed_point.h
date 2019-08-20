a2ddf19660c4b51c9751d74c07e47f40f13f560e
Christian Fulljames <christianfulljames@gmail.com>
2019-08-20 18:17:02 -0400

    Fix casts in fixed point conversions
    Some issues showed up when testing on an eight-bit ATmega328P due to the
    different default integer sizes.  The type casts have been fixed to make
    the code more portable.

--- src/fixed_point.h
+++ src/fixed_point.h
@@ -33,13 +33,13 @@ typedef uint32_t FixedPoint_t;
 #define FIXED_POINT_SHIFT (16U)
 
 //! Value 1 in fixed point.
-#define FIXED_POINT_FACTOR (1 << FIXED_POINT_SHIFT)
+#define FIXED_POINT_FACTOR ((FixedPoint_t)1 << FIXED_POINT_SHIFT)
 
 //! Mask for the fractional portion
 #define FIXED_POINT_MASK (FIXED_POINT_FACTOR - 1)
 
 //! Convert an integer value to a fixed point value.
-#define FIXED_POINT_FROM_INT(a) ((FixedPoint_t)((a) << FIXED_POINT_SHIFT))
+#define FIXED_POINT_FROM_INT(a) (((FixedPoint_t)(a) << FIXED_POINT_SHIFT))
 
 //! Convert a fixed point value to an integer via truncation.
 #define FIXED_POINT_TO_INT(x) ((uint16_t)(x >> FIXED_POINT_SHIFT))
